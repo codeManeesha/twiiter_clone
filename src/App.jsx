ec08aa80470390949f40961e6e1cb9b2e6ebb65c
krushnagaikwad07 <krushnagaikwad7799@gmail.com>
2023-05-05 15:19:44 +0530

    signup and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route ,Routes } from 'react-router'
 import Home from './pages/home/Home'
-import Login from './pages/login/login'
-import Signup from './pages/signup/signup'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
 
 function App() {
 
